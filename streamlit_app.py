836930170e6e7c1921d0a2a76da7046de834eb2b
shourya-goenka <54571965+shourya-goenka@users.noreply.github.com>
2024-05-12 20:11:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,6 @@ if tran_date:
     month = selected_date.strftime("%B")
     year = selected_date.year
 
-    st.write("Month:", month)
-    st.write("Year:", year)
-
 trans_type = st.selectbox("Select transaction type", ["Bank Transfer", "Bill Payment", "Investment", "Other", "Purchase", "Refund", "Subscription"])
 pmt_gateway = st.selectbox("Select payment gateway", ["CRED", "Google Pay", "HDFC", "ICICI UPI", "IDFC UPI", "Other", "Paytm", "PhonePe", "Razor Pay"])
 tran_state=st.selectbox("Select transaction state",['Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'])
