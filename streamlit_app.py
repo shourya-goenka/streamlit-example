4500fd72ef3204ca355d39de19b9d260c0ccbbcb
shourya-goenka <54571965+shourya-goenka@users.noreply.github.com>
2024-05-12 12:56:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,40 +21,17 @@ def validate_time_format(input_time):
 
 tran_date = st.date_input("Select the date of your transaction", datetime.date.today())
 tran_time = st.text_input("Enter transaction time", "in (HH:MM:SS) format")
-# Validate time and display error message if format is incorrect
-if not validate_time_format(tran_time):
+if (tran_time!="in (HH:MM:SS) format"):
+    # Validate time and display error message if format is incorrect
+    if not validate_time_format(tran_time):
     st.error("format error")
-# Display the entered time if format is correct
-else:
-    st.write("You entered:", tran_time)
+    # Display the entered time if format is correct
+    else:
+        st.write("You entered:", tran_time)
 merch_id = st.text_input("Enter the merchant's id:")
 cust_id = st.text_input("Enter the customer's id:")
 device_id = st.text_input("Enter the device's id:")
 trans_type = st.selectbox("Select transaction type", ["Bank Transfer", "Bill Payment", "Investment", "Other", "Purchase", "Refund", "Subscription"])
 pay_type = st.selectbox("Select payment gateway", ["CRED", "Google Pay", "HDFC", "ICICI UPI", "IDFC UPI", "Other", "Paytm", "PhonePe", "Razor Pay"])
 tran_state=st.selectbox("Select transaction state",['Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'])
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-merch_id = st.text_input("Enter the merchant's id:")
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+#merch_id = st.text_input("Enter the merchant's id:")
