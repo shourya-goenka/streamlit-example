f665ace072d02dbbcbcfe386366e182f5b101f5e
shourya-goenka <54571965+shourya-goenka@users.noreply.github.com>
2024-05-14 14:33:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,6 +94,7 @@ if button_clicked:
             results = []
             for index, row in df.iterrows():
                 input = []
+                st.write(row.values)
                 input.append(row.values[0])
                 input.append(row.values[1])
                 input.append(row.values[2])
