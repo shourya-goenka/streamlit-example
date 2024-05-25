b99dd3921d48a1943c0e86d7b2764140cc069565
shourya-goenka <54571965+shourya-goenka@users.noreply.github.com>
2024-05-25 22:30:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import pickle
 from xgboost import XGBClassifier
 
 """
-# Hello to your own UPI Transaction Fraud Detector!
+# Welcome to your own UPI Transaction Fraud Detector!
 
 You have the option of inspecting a single transaction by adjusting the parameters below OR you can even check 
 multiple transactions at once by uploading a .csv file in the specified format
